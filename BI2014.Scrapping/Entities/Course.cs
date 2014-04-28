4e1f333e40270513ac4360a070051ae9b9aa96e0
Armell <armell.claeys@outlook.com>
2014-04-28 15:25:36 +0200

    update 1

---
+++ BI2014.Scrapping/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Entities
+{
+    public class Course
+    {
+        public string Periode { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Level { get; set; }
+        public string year { get; set; }
+        public string Students { get; set; }
+
+    }
+}

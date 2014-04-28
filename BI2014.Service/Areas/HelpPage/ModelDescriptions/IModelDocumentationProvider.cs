5c0edff4bb4bbdb05ed0960a21f3bfc26ddc4d7d
Armell <armell.claeys@outlook.com>
2014-04-28 13:35:52 +0200

    init

---
+++ BI2014.Service/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace BI2014.Service.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}

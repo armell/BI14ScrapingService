f5e8775e4a015c763c7c8a1f17cc69d4a4917997
Armell <armell.claeys@outlook.com>
2014-04-28 13:35:52 +0200

    init

---
+++ BI2014.Service/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BI2014.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}

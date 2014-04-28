f39a9438399563a9240f99e38cfad3f6fe416a38
Armell <armell.claeys@outlook.com>
2014-04-28 23:15:46 +0200

    update members

---
+++ BI2014.Scrapping/Entities/Publication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Entities
+{
+    public class Publication
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+}

b7c458c32936b39f91c96707d00bc859c3353ef6
Armell <armell.claeys@outlook.com>
2014-04-28 15:25:36 +0200

    update 1

---
+++ BI2014.Scrapping/Engine/Parser.cs
@@ -0,0 +1,29 @@
+﻿using BI2014.Scrapping.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Engine
+{
+    public class Parser
+    {
+        public enum Provider {UUCS};
+        private Swagger _swagger = new Swagger();
+
+        public ICollection<Entities.Course> GetCourses(Provider provider)
+        {
+            IProvider sprovider = null;
+
+            switch(provider)
+            {
+                case Provider.UUCS:
+                    sprovider = new UUCSPRovider();
+                    sprovider.URI = @"http://www.cs.uu.nl/education/rooster.php?stijl=2&stjaar=a";
+                    break;
+            }
+            return sprovider.Courses;
+        }
+    }
+}
---
+++ BI2014.Scrapping/Provider/IProvider.cs
@@ -0,0 +1,15 @@
+﻿using BI2014.Scrapping.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Provider
+{
+    interface IProvider
+    {
+        string URI { get; set; }
+        ICollection<Course> Courses { get; set; }
+    }
+}

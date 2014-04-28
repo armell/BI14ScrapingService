5e84f2264c97059a9dd010a1220df02f932429d8
Armell <armell.claeys@outlook.com>
2014-04-28 15:25:36 +0200

    update 1

---
+++ BI2014.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using BI2014.Scrapping.Engine;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser webparser = new Parser();
+
+            var courses = webparser.GetCourses(Parser.Provider.UUCS);
+
+            Console.WriteLine(JsonConvert.SerializeObject(courses.Select(p => p.Code)));
+
+            System.Console.ReadLine();
+        }
+    }
+}

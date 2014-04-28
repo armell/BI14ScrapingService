e110ee91d27deb4d34d98e8ac1912c3f1065c08c
Armell <armell.claeys@outlook.com>
2014-04-28 13:35:52 +0200

    init

---
+++ BI2014.Service/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace BI2014.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ BI2014.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BI2014.Service.WebApiApplication" Language="C#" %>

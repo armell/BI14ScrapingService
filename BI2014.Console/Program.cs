c7fabfb2dfa42929f859a755c132f5eed7b965c6
Armell <armell.claeys@outlook.com>
2014-04-30 20:08:58 +0200

    URI courses UPDATE

--- BI2014.Console/Program.cs
+++ BI2014.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BI2014.Scrapping.Engine;
+using BI2014.Scrapping.Entities;
+using MongoDB.Driver.Builders;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,7 +20,17 @@ namespace BI2014.ConsoleTest
 
             //Console.WriteLine(JsonConvert.SerializeObject(courses.Select(p => new { Code = p.Code, Name = p.Name, URI = p.URI })));
 
-            Console.WriteLine(JsonConvert.SerializeObject(webparser.GetMembers(Parser.Provider.UUCS)));
+            //Console.WriteLine(JsonConvert.SerializeObject(webparser.GetMembers(Parser.Provider.UUCS)));
+
+            BI2014.Scrapping.Mongo.MongoService<Course> db = new Scrapping.Mongo.MongoService<Course>();
+            foreach (var item in db.Database.GetCollection<Course>("courses").FindAll())
+            {
+                var query = Query.EQ("URI", item.URI);
+                var toReplace = item.URI.Replace("stijl=2&","");
+                var update = Update.Set("URI", toReplace);
+                db.Database.GetCollection<Member>("courses").Update(query, update);
+
+            }
             System.Console.ReadLine();
         }
     }

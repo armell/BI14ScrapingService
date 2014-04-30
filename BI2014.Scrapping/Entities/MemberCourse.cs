2cdff98b115fe2e620596fdb8788eb3b3830738c
Armell <armell.claeys@outlook.com>
2014-04-30 18:00:50 +0200

    member_course

---
+++ BI2014.Scrapping/Entities/MemberCourse.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Entities
+{
+    public class MemberCourse
+    {
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+        public string MemberURI { get; set; }
+        public string CourseURI { get; set; }
+        public string Year { get; set; }
+        public string Weight { get; set; }
+        public string Periode { get; set; }
+    }
+}

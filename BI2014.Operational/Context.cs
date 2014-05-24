dae03f7cc4cc1c254f3fdb7c5dce86f84e5fd5ca
Armell <armell.claeys@outlook.com>
2014-05-24 16:50:09 +0200

    UU provider added to GIT

---
+++ BI2014.Operational/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Operational
+{
+    public class AzureContext:DbContext
+    {
+
+        public AzureContext()
+            : base("BIDW")
+        {
+
+        }
+    }
+}

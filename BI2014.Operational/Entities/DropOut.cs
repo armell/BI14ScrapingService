a8dba0aa74a449564f6aa6839e92cce6532174e7
Armell <armell.claeys@outlook.com>
2014-05-24 16:50:09 +0200

    UU provider added to GIT

---
+++ BI2014.Operational/Entities/DropOut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Operational.Entities
+{
+    public class DropOut
+    {
+        public string Year { get; set; }
+    }
+}

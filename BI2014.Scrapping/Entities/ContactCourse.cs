f8da4b82845a9ef24c0064ebaac14f4861f88433
Armell <armell.claeys@outlook.com>
2014-05-27 15:35:59 +0200

    some more bugs

---
+++ BI2014.Scrapping/Entities/ContactCourse.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Entities
+{
+    public class ContactCourse
+    {
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+        public string URI { get; set; }
+        public string Category { get; set; }
+        public string Contact { get; set; }
+        public string Extracted { get; set; }
+    }
+}

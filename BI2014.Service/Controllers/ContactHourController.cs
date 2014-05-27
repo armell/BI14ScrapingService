cb1e5a456c3fc9aceec1636f24168bb7f319aac4
Armell <armell.claeys@outlook.com>
2014-05-27 15:26:13 +0200

    Contact hours v1

---
+++ BI2014.Service/Controllers/ContactHourController.cs
@@ -0,0 +1,27 @@
+﻿using BI2014.Scrapping.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BI2014.Service.Controllers
+{
+    public class ContactHourController : ApiController
+    {
+        private Parser _parser = new Parser();
+        // GET: api/MemberCourse/5
+        [ActionName("mongo")]
+        public HttpResponseMessage GetMongo()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, _parser.GetContact(Parser.Provider.LOCAL));
+        }
+
+        [ActionName("uucs")]
+        public HttpResponseMessage GetUUCS()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, _parser.GetContact(Parser.Provider.UUCS));
+        }
+    }
+}

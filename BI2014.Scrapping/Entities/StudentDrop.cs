c04c564828e8e7ebe9fc693ccaa9ba9b5c4954a2
Armell <armell.claeys@outlook.com>
2014-05-29 22:39:40 +0200

    add Jakob JSON output dropout

---
+++ BI2014.Scrapping/Entities/StudentDrop.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI2014.Scrapping.Entities
+{
+    public class StudentDrop
+    {
+        //[JsonIgnore]
+        //public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string study { get; set; }
+        public string level { get; set; }
+        public string classic_level { get; set; }
+        public string start_year {get; set;}
+        public string prop_date { get; set; }
+
+    }
+}

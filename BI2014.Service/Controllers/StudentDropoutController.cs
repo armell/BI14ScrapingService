edbbe57771970583fa50b8ac0cb20a342e5d47a0
Armell <armell.claeys@outlook.com>
2014-05-29 22:39:40 +0200

    add Jakob JSON output dropout

---
+++ BI2014.Service/Controllers/StudentDropoutController.cs
@@ -0,0 +1,28 @@
+﻿using BI2014.Scrapping.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace BI2014.Service.Controllers
+{
+    public class StudentDropoutController : ApiController
+    {
+         [ActionName("mongo")]
+        public HttpResponseMessage GetMongo()
+        {
+             //retrieve Jakob's Json output
+             var path = HttpContext.Current.Server.MapPath("~/App_Data");
+             var jsonOutput = File.ReadAllText(path + "/stud_drop_out.json");
+             var listOutput = JsonConvert.DeserializeObject<List<StudentDrop>>(jsonOutput);
+
+             return Request.CreateResponse(listOutput);
+        }
+    }
+}
